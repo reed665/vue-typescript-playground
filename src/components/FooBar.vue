723bf06d54f372d8fdeb34c0af58420c487ff5f8
reed665 <abuiko@outlook.com>
2020-04-19 08:51:47 +0300

    FooBar component with propsgit add .

---
+++ src/components/FooBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="FooBar">
+    <h3>
+      Hello from FooBar component!!
+    </h3>
+
+    <p>
+      {{ foo }}
+    </p>
+
+    <p>{{ theNumber }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FooBar extends Vue {
+  @Prop({ type: String, required: false, default: 'bar' })
+  private foo: string | undefined;
+
+  private theNumber?: number;
+
+  created() {
+    this.theNumber = 42;
+  }
+
+}
+</script>

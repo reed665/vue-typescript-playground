f28002e7b38d5c245a256bdd1bab4a5db315080b
reed665 <abuiko@outlook.com>
2020-04-20 12:42:10 +0300

    FullName component

---
+++ src/components/FullName.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="full-name">
+    <div class="first-name">{{ fullName }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+const reverseString = (str: string): string => {
+  return str.split('').reverse().join('');
+}
+
+@Component
+export default class FullName extends Vue {
+  reversed = false;
+
+  @Prop({ required: true })
+  private readonly firstName!: string;
+
+  @Prop({ default: '' })
+  private readonly lastName?: string;
+
+  get fullName(): string {
+    let name = this.firstName;
+    if (this.lastName) {
+      name += ` ${this.lastName}`;
+    }
+    return (this.reversed) ? reverseString(name) : name;
+  }
+
+  public reverse(): void {
+    this.reversed = !this.reversed;
+  }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Hello from Vue.js + Typescript App" />
+    <FullName ref="fullName" first-name="James" last-name="Bond" />
+    <button @click="reverseFullName">Reverse full name</button>
   </div>
 </template>
 
-<script>
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import FullName from '@/components/FullName.vue';
 
-export default {
-  name: 'Home',
+@Component({
   components: {
-    HelloWorld
+    FullName,
   }
+})
+export default class Home extends Vue {
+  $refs!: {
+    fullName: FullName;
+  }
+
+  reverseFullName(): void {
+    this.$refs.fullName.reverse();
+  }
+
 }
 </script>
